4124839ea473a8a50aadcbce2f693a783178604a
Balint HAUI <balint.haui-external@rbinternational.com>
2020-08-12 16:16:12 +0200

    forbiddenNamesValidator.bind(this)

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   title = 'OFSA Filters Playground';
   genders: string[] = ['male', 'female', 'not sure'];
   signupForm: FormGroup;
+  forbiddenNames: string[] = ['Koko', 'Poko', 'Moko'];
 
   onSubmit() {
     console.log(this.signupForm);
@@ -27,11 +28,23 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNamesValidator.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('female'),
       hobbies: new FormArray([]),
     });
+  }
+
+  // my custom validator
+  forbiddenNamesValidator(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.includes(control.value)) {
+      return { nameIsForbidden: true };
+    } else {
+      return null;
+    }
   }
 }
