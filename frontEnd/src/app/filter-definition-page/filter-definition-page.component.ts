84cc86fd8f23c112faf807a24299c5b1b9590658
Balint HAUI <balint.haui-external@rbinternational.com>
2020-08-13 15:29:30 +0200

    progress

--- frontEnd/src/app/filter-definition-page/filter-definition-page.component.ts
+++ frontEnd/src/app/filter-definition-page/filter-definition-page.component.ts
@@ -25,6 +25,24 @@ export class FilterDefinitionPageComponent implements OnInit {
 
   accessTypeOptions = ['readOnly', 'readWrite'];
 
+  filterTypes = [
+    { id: 1, name: 'Hierarchy' },
+    { id: 2, name: 'Data Element' },
+    { id: 3, name: 'Group' },
+  ];
+
+  dimensions = [
+    { id: 1, name: 'DIM1' },
+    { id: 2, name: 'DIM2' },
+    { id: 3, name: 'DIM3' },
+  ];
+
+  hierarchies = [
+    { id: 1, name: 'Products' },
+    { id: 2, name: 'Loans' },
+    { id: 3, name: 'GoldMines' },
+  ];
+
   onSubmit() {
     console.log(this.filterDefinitionForm);
   }
@@ -37,8 +55,13 @@ export class FilterDefinitionPageComponent implements OnInit {
         accessType: new FormControl(),
         description: new FormControl(),
       }),
-      filterTypeSelection: new FormGroup({}),
-      hierarchyFilters: new FormGroup({}),
+      filterTypeSelection: new FormGroup({
+        filterType: new FormControl(),
+      }),
+      hierarchyFilters: new FormGroup({
+        dimension: new FormControl(),
+        hierarchy: new FormControl(),
+      }),
     });
   }
 }
