d2d9b88c8a4d8c283ef7c0329db840f4b1420b7a
Balint HAUI <balint.haui-external@rbinternational.com>
2020-08-17 13:37:44 +0200

    progress

--- frontEnd/src/app/filters-main-page/filters-main-page.component.ts
+++ frontEnd/src/app/filters-main-page/filters-main-page.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface FilterRecord {
+  id: number;
+  name: string;
+  folder: string;
+  type: string;
+  modificationDate: string;
+  modifiedBy: string;
+}
 
 @Component({
   selector: 'app-filters-main-page',
@@ -15,19 +26,94 @@ export class FiltersMainPageComponent implements OnInit {
     { id: 4, name: 'Folder-4' },
     { id: 5, name: 'Folder-5' },
   ];
+
   filterType = [
     { id: 1, name: 'Hierarchy' },
     { id: 2, name: 'Data Element' },
     { id: 3, name: 'Group' },
   ];
 
-  filterTableData = [
-    { id: 1, name: 'Filter-1', folder: 'Folder-2', type: 'Hierarchy' },
-    { id: 2, name: 'Filter-2', folder: 'Folder-2', type: 'Hierarchy' },
-    { id: 3, name: 'Filter-3', folder: 'Folder-2', type: 'Data Element' },
-    { id: 4, name: 'Filter-4', folder: 'Folder-1', type: 'Group' },
-    { id: 5, name: 'Filter-5', folder: 'Folder-3', type: 'Hierarchy' },
+  FILTER_TABLE_DATA = [
+    {
+      id: 1,
+      name: 'Filter-1',
+      folder: 'Folder-2',
+      type: 'Hierarchy',
+      modificationDate: '2020-01-01',
+      modifiedBy: 'Gustavo',
+    },
+    {
+      id: 2,
+      name: 'Filter-2',
+      folder: 'Folder-2',
+      type: 'Hierarchy',
+      modificationDate: '2020-01-01',
+      modifiedBy: 'Karlson',
+    },
+    {
+      id: 3,
+      name: 'Filter-3',
+      folder: 'Folder-2',
+      type: 'Data Element',
+      modificationDate: '2020-01-01',
+      modifiedBy: 'Klaus',
+    },
+    {
+      id: 4,
+      name: 'Filter-4',
+      folder: 'Folder-1',
+      type: 'Group',
+      modificationDate: '2020-01-01',
+      modifiedBy: 'Heimann',
+    },
+    {
+      id: 5,
+      name: 'Filter-5',
+      folder: 'Folder-3',
+      type: 'Hierarchy',
+      modificationDate: '2020-01-01',
+      modifiedBy: 'FitzPatrik',
+    },
   ];
+
+  displayedColumns: string[] = [
+    'name',
+    'folder',
+    'type',
+    'modificationDate',
+    'modifiedBy',
+  ];
+
+  dataSource = new MatTableDataSource<FilterRecord>(this.FILTER_TABLE_DATA);
+  selection = new SelectionModel<FilterRecord>(true, []);
+
+  /* NG MATERIAL DATA TABLE IMPORTS START ***************************************************/
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: FilterRecord): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.id + 1
+    }`;
+  }
+
+  /* NG MATERIAL DATA TABLE IMPORTS END ***************************************************/
 
   onFiltersSearchFormSubmit() {
     console.log(this.filtersSearchForm);
